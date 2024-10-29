e4acdc8880ac5498c69aaa938912136aad5051b9
Mateo-Paternina-Mercado <185979424+Mateo-Paternina-Mercado@users.noreply.github.com>
2024-10-29 12:06:06 -0500

    alfin

---
+++ main.py
@@ -0,0 +1,11 @@
+fullName = "Mateo Paternina Mercado"   #Tipo de dato string
+gender = "M"  #Tipo de dato char
+age = 22   #Tipo de dato int
+height = 1.86    #Tipo de doble
+trainer = False    #Tipo de dato boolean
+
+# conjuntos
+hobbies = ["Programar", 'jugar', """escuchar musica"""] #Tipo de dato array
+direction = (dict) ["country": "Colombia", "state": "Santander"] #Tipo de dato object
+
+print(f"Student's Name: {fullName}")
